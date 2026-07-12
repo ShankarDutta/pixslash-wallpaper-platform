5750ca61dbe4623e96524b72e97260f812d7f301
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 23:09:33 +0530

    refactor: update theme toggle button size and className handling

--- src/components/Buttons/ThemeToggleButton.tsx
+++ src/components/Buttons/ThemeToggleButton.tsx
@@ -1,23 +1,28 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { MoonStarIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
-const ThemeToggleButton = () => {
+type ThemeToggleButtonProps = {
+  classname?: string;
+};
+
+const ThemeToggleButton = ({ classname }: ThemeToggleButtonProps) => {
   const { theme, setTheme } = useTheme();
 
   return (
     <button
       type="button"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="flex cursor-pointer items-center">
+      className={cn(`flex cursor-pointer items-center ${classname}`)}>
       <SunIcon
-        size={28}
+        size={20}
         className="-rotate-90 opacity-100 transition-all duration-300 dark:rotate-0 dark:opacity-0"
       />
 
       <MoonStarIcon
-        size={28}
+        size={20}
         className="absolute -rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100"
       />
     </button>
