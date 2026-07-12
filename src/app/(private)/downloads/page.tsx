703585c404bcb589cd62012d0b90558a0b727415
manasi sadhu <ms.manasisadhu@gmail.com>
2026-07-12 08:53:23 +0530

    feat: add likes, downloads & category route

---
+++ src/app/(private)/downloads/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="grid h-dvh place-items-center">
+      All Downloads Wallpapers
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/(private)/likes/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="grid h-dvh place-items-center">all liked wallpapers</div>
+  );
+};
+
+export default page;
