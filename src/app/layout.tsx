e86fcfefd581010da1284d06b5fc7683f13f9996
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-07-15 15:48:25 +0530

    Add TooltipProvider to root layout
    Wrap the main content with TooltipProvider from shadcn/ui to enable tooltip functionality across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import MobileBottomNav from "@/components/Header/MobileBottomNav";
 import PublicHeader from "@/components/Header/PublicHeader";
 import ThemeProvider from "@/components/Providers/ThemeProvider";
+import { TooltipProvider } from "@/components/shadcnui/tooltip";
 import { notoSansHeading, nunitoSans } from "@/lib/fonts";
 import { RootLayoutProps } from "@/lib/type";
 import { cn } from "@/lib/utils";
@@ -23,7 +24,9 @@ const RootLayout = ({ children }: RootLayoutProps) => {
           defaultTheme="dark"
           enableSystem={false}>
           <PublicHeader />
-          <main className="mx-auto max-w-7xl pb-16 lg:pb-0">{children}</main>
+          <TooltipProvider>
+            <main className="mx-auto max-w-7xl pb-16 lg:pb-0">{children}</main>
+          </TooltipProvider>
           <MobileBottomNav />
         </ThemeProvider>
       </body>
